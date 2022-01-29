690cdcf1e0435f11c6e6957e9b15e7c5b19f5022
Seema Ullal <seemaullal@gmail.com>
2022-01-29 06:46:14 +0000

    Define function to make code less repetitive; still has bugs

--- produce_summary.py
+++ produce_summary.py
@@ -1,14 +1,17 @@
-print("Day 1")
-the_file = open("um-deliveries-day-1.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+def print_info_from_file(line_text):
+    line = line_text.rstrip()
+    words = line_text.split('|')
 
     melon = words[0]
     count = words[0]
     amount = words[0]
-
     print(f"Delivered {count} {melon}s for total of ${amount}")
+
+
+print("Day 1")
+the_file = open("um-deliveries-day-1.txt")
+for line in the_file:
+    print_info_from_file(line)
 the_file.close()
 
 
